e3eca1c8860a3d88ff9ffa6c92b89efba2a75d8f
HirokazuHara <h-hara@7th-stage.com>
2020-08-13 13:40:37 +0900

    RO以外のページでは実行できないように修正

--- popup.js
+++ popup.js
@@ -2,6 +2,10 @@ $(function () {
   $("#btn").click(function () {
     // Contentsにイベントを送信する。
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (!tabs[0].url.startsWith('https://ragnarokonline.gungho.jp/')) {
+        $("body").html(`<font color='red'>このページでは実行できません。</font>`);
+        return;
+      }
       chrome.tabs.sendMessage(tabs[0].id, {}, function (response) {});
     });
   });
